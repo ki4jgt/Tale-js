fd3183db050b93d3e17099efbf5d46318a021d75
Jesse <ki4jgt@gmail.com>
2022-04-09 09:09:58 -0500

    added input support

--- tale.js
+++ tale.js
@@ -14,12 +14,20 @@ async function load_book(url){
     window.location.hash = "index"
 }
 
+function goto(page){
+    window.location.hash = page
+}
+
 function clear(){
     page.innerHTML = ""
 }
 
 function background(url){
     document.body.style.backgroundImage = "url('" + url + "')"
+}
+
+function input(name, comment){
+    page.innerHTML += "<input type = 'text' name = '" + name + "' placeholder = '" + comment + "' onkeyup = 'form_handler(event)'>"
 }
 
 function set(key, value){
@@ -30,11 +38,13 @@ function get(key){
     return localStorage.getItem(key);
 }
 
-function goto(page){
-    window.location.hash = page
+window.onhashchange = function () {
+    eval(book[window.location.hash.replace("#", "")])
 }
 
-window.onhashchange = function () {
-    console.log(book)
-    eval(book[window.location.hash.replace("#", "")])
+function form_handler(event){
+    if (event.key == "Enter"){
+        set(event.target.name, event.target.value)
+        event.target.setAttribute("disabled", true);
+    }
 }
